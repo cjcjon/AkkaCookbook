12efc23f7be3f0c14ed4205718bc0f3bbb82702e
cjcjon <cjcjon30@gmail.com>
2022-11-12 15:14:20 +0900

    feat: Future callback 실습

---
+++ src/main/scala/com/packt/chapter4/callback.scala
@@ -0,0 +1,16 @@
+package com.packt.chapter4
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+object callback extends App {
+  val future = Future(1 + 2).mapTo[Int]
+
+  future onComplete {
+    case Success(result) => println(s"result is $result")
+    case Failure(fail) => fail.printStackTrace()
+  }
+
+  println("Executed before callback")
+}
